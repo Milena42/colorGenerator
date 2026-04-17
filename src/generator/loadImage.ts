ee133c68ecf1feffa32162b70e0e9841559793f2
Milena <milena.ev.dmitrieva@gmail.com>
2026-04-17 18:38:56 +0300

    refactor: выделит загрузку цветов из картинки в библиотеку к генератору

---
+++ src/generator/loadImage.ts
@@ -0,0 +1,94 @@
+import chroma from 'chroma-js';
+import { Color, type ColorMap } from './common';
+
+export function mapFromImage(
+    img: HTMLImageElement,
+    canvas: HTMLCanvasElement,
+    ctx: CanvasRenderingContext2D,
+): ColorMap {
+    ////////////////////////////////
+    // уменьшаем большие картинки: для скорости и борьбы с зернистостью
+    // возможна проблема с размыванием векторных покрасов => возвращаясь к проблеме золотого картона
+    let theWidth: number;
+    let theHeight: number;
+    const resizeImage = true;
+    if (resizeImage) {
+        const maxSize = 1000;
+        const maxNatural = Math.max(img.naturalHeight, img.naturalWidth);
+        let k = 0.9;
+        if (maxNatural > maxSize) {
+            k = maxSize / maxNatural;
+        }
+        theWidth = Math.round(img.naturalWidth * k);
+        theHeight = Math.round(img.naturalHeight * k);
+    } else {
+        theWidth = img.naturalWidth;
+        theHeight = img.naturalHeight;
+    }
+    ////////////////
+
+    canvas.width = theWidth;
+    canvas.height = theHeight;
+
+    ctx.drawImage(img, 0, 0, theWidth, theHeight); // TODO InvalidStateError, TypeMismatchError
+    const imgData = ctx.getImageData(0, 0, theWidth, theHeight); // TODO IndexSizeError, SecurityError
+
+    const pixels = imgData.data; /// одномерный массив rgbargbargba
+
+    return mapFromPixels(pixels);
+}
+
+/***
+ * @param pixels rgbargbargba array
+ */
+export function mapFromPixels(pixels: Uint8ClampedArray<ArrayBuffer>): ColorMap {
+    /* обрабатываем массив rgbargbargba - по 4 элемента на каждый пиксель */
+
+    const newImgMap: ColorMap = {
+        data: new Map(),
+        totalQ: pixels.length / 4,
+    };
+
+    for (let i = 0; i < pixels.length; i += 4) {
+        let r = pixels[i];
+        let g = pixels[i + 1];
+        let b = pixels[i + 2];
+
+        const a = pixels[i + 3];
+        if (a != 255) {
+            // прозрачные и полупрозрачные пиксели не считаются
+            continue;
+        }
+
+        // TODO мб убрать, это режет цвета для скорости
+        if (r <= 240) r = Math.round(r / 20) * 20;
+        else r = 255;
+        if (g <= 240) g = Math.round(g / 20) * 20;
+        else g = 255;
+        if (b <= 240) b = Math.round(b / 20) * 20;
+        else b = 255;
+        //
+
+        const rgb = chroma(r, g, b, 'rgb');
+        const colorString = rgb.hex();
+
+        if (!newImgMap.data.has(colorString)) {
+            let [l, c, h] = rgb.oklch();
+
+            if (!l) l = 0;
+            if (!c) c = 0;
+            if (!h) h = 0;
+            // потому что у меня в процентах, а не доля от 1
+            l *= 100;
+            c *= 100;
+
+            const elem = new Color(l, { c, h }, 1);
+
+            newImgMap.data.set(colorString, elem);
+        } else {
+            const elem = newImgMap.data.get(colorString)!;
+            elem.q += 1;
+        }
+    }
+    return newImgMap;
+}
--- src/generatorFromPicturePage/InputPicture.vue
+++ src/generatorFromPicturePage/InputPicture.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import IconClose from '@/assets/icons/IconClose.vue';
-import { Color, type ColorMap } from '@/generator/common';
-import chroma from 'chroma-js';
+import { type ColorMap } from '@/generator/common';
+import { mapFromImage } from '@/generator/loadImage';
 import { onMounted, ref } from 'vue';
 
 const imgMap = defineModel<ColorMap | undefined>('imgMap');
@@ -56,96 +56,15 @@ function drop(e: DragEvent) {
     reader.readAsDataURL(file);
 }
 
-function fillMapFromImg(pixels: Uint8ClampedArray<ArrayBuffer>) {
-    /* обрабатываем массив rgbargbargba - по 4 элемента на каждый пиксель */
-
-    const newImgMap: ColorMap = {
-        data: new Map(),
-        totalQ: pixels.length / 4,
-    };
-
-    for (let i = 0; i < pixels.length; i += 4) {
-        let r = pixels[i];
-        let g = pixels[i + 1];
-        let b = pixels[i + 2];
-
-        const a = pixels[i + 3];
-        if (a != 255) {
-            // прозрачные и полупрозрачные пиксели не считаются
-            continue;
-        }
-
-        // TODO мб убрать, это режет цвета для скорости
-        if (r <= 240) r = Math.round(r / 20) * 20;
-        else r = 255;
-        if (g <= 240) g = Math.round(g / 20) * 20;
-        else g = 255;
-        if (b <= 240) b = Math.round(b / 20) * 20;
-        else b = 255;
-        //
-
-        const rgb = chroma(r, g, b, 'rgb');
-        const colorString = rgb.hex();
-
-        if (!newImgMap.data.has(colorString)) {
-            let [l, c, h] = rgb.oklch();
-
-            if (!l) l = 0;
-            if (!c) c = 0;
-            if (!h) h = 0;
-            // потому что у меня в процентах, а не доля от 1
-            l *= 100;
-            c *= 100;
-
-            const elem = new Color(l, { c, h }, 1);
-
-            newImgMap.data.set(colorString, elem);
-        } else {
-            const elem = newImgMap.data.get(colorString)!;
-            elem.q += 1;
-        }
-    }
-    return newImgMap;
-}
-
 function loadImgData(e: Event) {
     const img1: HTMLImageElement = e.target as HTMLImageElement;
 
-    ////////////////////////////////
-    // уменьшаем большие картинки: для скорости и борьбы с зернистостью
-    // возможна проблема с размыванием векторных покрасов => возвращаясь к проблеме золотого картона
-    let theWidth: number;
-    let theHeight: number;
-    const resizeImage = true;
-    if (resizeImage) {
-        const maxSize = 1000;
-        const maxNatural = Math.max(img1.naturalHeight, img1.naturalWidth);
-        let k = 0.9;
-        if (maxNatural > maxSize) {
-            k = maxSize / maxNatural;
-        }
-        theWidth = Math.round(img1.naturalWidth * k);
-        theHeight = Math.round(img1.naturalHeight * k);
-    } else {
-        theWidth = img1.naturalWidth;
-        theHeight = img1.naturalHeight;
-    }
-    ////////////////
-
-    if (!ctx.value || !canvas.value) {
-        console.log('no canvas?');
+    if (!canvas.value || !ctx.value) {
+        console.error('canvas not found');
         return;
     }
 
-    canvas.value.width = theWidth;
-    canvas.value.height = theHeight;
-
-    ctx.value.drawImage(img1, 0, 0, theWidth, theHeight); // TODO InvalidStateError, TypeMismatchError
-    const imgData = ctx.value.getImageData(0, 0, theWidth, theHeight); // TODO IndexSizeError, SecurityError
-
-    const pixels = imgData.data; /// одномерный массив rgbargbargba
-
-    imgMap.value = fillMapFromImg(pixels);
+    imgMap.value = mapFromImage(img1, canvas.value, ctx.value);
 
     emit('change');
 }
